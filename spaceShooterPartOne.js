36368432d3f05a565d0d2cfaad511c671a6153d2
conor <conorbudge555@gmail.com>
2016-10-25 10:35:35 -0500

    Added pool for drawing bullets

--- spaceShooterPartOne.js
+++ spaceShooterPartOne.js
@@ -33,8 +33,31 @@ var ImageRepository = new function () {
     // Define images
     this.empty = null;
     this.background = new Image();
+    this.spaceShip = new Image();
+    this.bullet = new Image();
+    //Check if images have loaded before game
+    var numImages = 3;
+    var numLoaded = 0;
+    function imageLoaded() {
+        numLoaded++;
+        if (numLoaded === numImages) {
+            window.init();
+        }
+    }
+    this.background.onload = function() {
+        imageLoaded();
+    }
+    this.spaceship.onload = function() {
+        imageLoaded();
+    }
+    this.bullet.onload = function() {
+        imageLoaded();
+    }
+
     // Set images src
     this.background.src = "img/bg.png";
+    this.spaceShip.src = "img/spaceShip.gif";
+    this.bullet.src = "img/bullet.png";
 };
 
 /*
@@ -45,6 +68,9 @@ function Drawable() {
         // Default vars
         this.x = x;
         this.y = y;
+        //Need to set the height and width of each object to it’s associated image.
+        this.width = width;
+        this.height = height;
     };
     this.speed = 0;
     this.canvasWidth = 0;
@@ -78,9 +104,74 @@ function Background() {
             this.y = 0;
     };
 }
-
 // Set Background to inherit properties from Drawable
 Background.prototype = new Drawable();
+
+
+
+/**
+ * Custom Pool object. Holds Bullet objects to be managed to prevent
+ * garbage collection.
+ *
+ * http://blog.sklambert.com/html5-canvas-game-the-player-ship/#the-player-ship
+ * http://gamedev.tutsplus.com/tutorials/implementation/object-pools-help-you-reduce-lag-in-resource-intensive-games/
+ */
+function Pool(maxSize) {
+    var size = maxSize; // Max bullets allowed in the pool
+    var pool = [];
+    /*
+     * Populates the pool array with Bullet objects
+     */
+    this.init = function() {
+        for (var i = 0; i < size; i++) {
+            // Initalize the bullet object
+            var bullet = new Bullet();
+            bullet.init(0,0, imageRepository.bullet.width,
+                imageRepository.bullet.height);
+            pool[i] = bullet;
+        }
+    };
+    /*
+     * Grabs the last item in the list and initializes it and
+     * pushes it to the front of the array.
+     */
+    this.get = function(x, y, speed) {
+        if(!pool[size - 1].alive) {
+            pool[size - 1].spawn(x, y, speed);
+            pool.unshift(pool.pop());
+        }
+    };
+    /*
+     * Used for the ship to be able to get two bullets at once. If
+     * only the get() function is used twice, the ship is able to
+     * fire and only have 1 bullet spawn instead of 2.
+     */
+    this.getTwo = function(x1, y1, speed1, x2, y2, speed2) {
+        if(!pool[size - 1].alive &&
+            !pool[size - 2].alive) {
+            this.get(x1, y1, speed1);
+            this.get(x2, y2, speed2);
+        }
+    };
+    /*
+     * Draws any in use Bullets. If a bullet goes off the screen,
+     * clears it and pushes it to the front of the array.
+     */
+    this.animate = function() {
+        for (var i = 0; i < size; i++) {
+            // Only draw until we find a bullet that is not alive
+            if (pool[i].alive) {
+                if (pool[i].draw()) {
+                    pool[i].clear();
+                    pool.push((pool.splice(i,1))[0]);
+                }
+            }
+            else
+                break;
+        }
+    };
+}
+
 
 
 /*
